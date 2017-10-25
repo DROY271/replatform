a596ad6d7c504264df2d96e3567a6927871de2d8
Roy <107406@AMB00681.local>
2017-10-25 18:18:39 +0530

    Read files

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -13,18 +13,13 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
-
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        Scanner scanner = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream(path)).useDelimiter("\\A");
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
